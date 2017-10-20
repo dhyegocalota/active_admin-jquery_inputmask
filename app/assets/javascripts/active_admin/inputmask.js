d8fee41ea9811febd8bd0707fec4f152c53f4db1
Dhyego Fernando <dhyegofernando@gmail.com>
2017-10-20 01:33:02 -0300

    Remove JS dependencies

--- app/assets/javascripts/active_admin/inputmask.js
+++ app/assets/javascripts/active_admin/inputmask.js
@@ -1,11 +1,4 @@
-//= require jquery
-//= require inputmask/inputmask
-//= require inputmask/jquery.inputmask
-//= require inputmask/inputmask.extensions
-//= require inputmask/inputmask.date.extensions
-//= require inputmask/inputmask.numeric.extensions
-//= require inputmask/inputmask.phone.extensions
-//= require inputmask/bindings/inputmask.binding
+//= require formtastic/inputmask
 
 (function($) {
   'use strict';
